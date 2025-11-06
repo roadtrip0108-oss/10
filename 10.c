468d830dd33b4076a54e27e70b55a3b3ba6eabb5
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-11-06 15:33:00 +0900

    10_4

--- 10.c
+++ 10.c
@@ -4,13 +4,10 @@
 
 int main(int argc, char *argv[])
 {
-  char src[] = "The worst things to eat before you sleep\n";
-  char dst[100];
+  char str[30] = "happy C programming";
   
-  strcpy(dst, src);
-  
-  printf("copied string : %s\n", dst);
-  
+  printf("문자열\"%s\"의 길이 : %i\n", str, strlen(str) );
+   
   system("PAUSE");	
   return 0;
 }
