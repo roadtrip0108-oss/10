da01a3c56766313f40214fc3c18407a396e47a9c
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-11-06 15:44:01 +0900

    10_5

--- 10.c
+++ 10.c
@@ -4,10 +4,19 @@
 
 int main(int argc, char *argv[])
 {
-  char str[30] = "happy C programming";
+  char input[100];
+  FILE* fp;
+  int i;
   
-  printf("문자열\"%s\"의 길이 : %i\n", str, strlen(str) );
-   
+  fp = fopen("sample.txt", "w");
+  
+  for(i=0;i<3;i++) {
+  printf("input a word: ");
+  scanf("%s", input);
+  fprintf(fp, "%s\n", input);
+}  
+  fclose(fp);
+  
   system("PAUSE");	
   return 0;
 }
